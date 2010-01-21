1557622de7a244347f89ea8b138f3ca621b47706
tsukkee <takayuki0510@gmail.com>
2010-01-21 21:57:52 +0900

    add license description (The MIT License)

--- autoload/lingr.py
+++ autoload/lingr.py
@@ -1,4 +1,30 @@
 # vim:set fileencoding=utf-8:
+# Lingr-Vim: Lingr client for Vim
+# Version:     0.0.1
+# Last Change: 2010 Jan 21
+# Author:      tsukkee <takayuki0510+lingr_vim at gmail.com>
+# Licence:     The MIT License {{{
+#     Permission is hereby granted, free of charge, to any person obtaining a copy
+#     of this software and associated documentation files (the "Software"), to deal
+#     in the Software without restriction, including without limitation the rights
+#     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#     copies of the Software, and to permit persons to whom the Software is
+#     furnished to do so, subject to the following conditions:
+#
+#     The above copyright notice and this permission notice shall be included in
+#     all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#     THE SOFTWARE.
+#
+#     日本語参考訳
+#     http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license
+# }}}
 
 # This code is based on lingr.rb at below URL
 # http://github.com/psychs/lingr-irc/blob/master/lingr.rb
