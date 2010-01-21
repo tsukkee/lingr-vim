8f66d28f9b8e5a429b0feaf186e957f216dd3f5b
tsukkee <takayuki0510@gmail.com>
2010-01-21 14:12:43 +0900

    update README

--- autoload/lingr.py
+++ autoload/lingr.py
@@ -221,8 +221,10 @@ class Connection(object):
         return res
 
     def say(self, room_id, text):
+        self._debug("requesting room/say: " + room_id + " " + text)
         res = self._post("room/say",\
             {"session": self.session, "room": room_id, "nickname": self.nickname, "text": text})
+        self._debug("room/say response: " + str(res))
         return res
 
     def observe(self):
