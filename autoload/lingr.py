4a0a13b29bb20949ebb08780f44f82f329905323
tsukkee <takayuki0510@gmail.com>
2010-06-22 21:30:28 +0900

    start to write documents

--- autoload/lingr.py
+++ autoload/lingr.py
@@ -21,9 +21,6 @@
 #     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #     THE SOFTWARE.
-#
-#     日本語参考訳
-#     http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license
 # }}}
 
 # This code is based on lingr.rb at below URL
